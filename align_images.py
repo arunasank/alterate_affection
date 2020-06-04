65ff5af0680d1a5ebc62ec5f95d7f9ff010d3cf0
ralcant <ralcanta@mit.edu>
2020-06-04 15:43:18 -0400

    changed alignmend of images to return quad values

--- align_images.py
+++ align_images.py
@@ -15,6 +15,31 @@ def unpack_bz2(src_path):
         fp.write(data)
     return dst_path
 
+def main(RAW_IMAGES_DIR, ALIGNED_IMAGES_DIR, create_if_necessary= False):
+    """
+    Extracts and aligns all faces from images using DLib and a function from original FFHQ dataset preparation step
+    python align_images.py /raw_images /aligned_images
+    """
+    if create_if_necessary: 
+        os.makedirs(RAW_IMAGES_DIR, exist_ok= True)
+        os.makedirs(ALIGNED_IMAGES_DIR, exist_ok=True)
+        print(f"paths {RAW_IMAGES_DIR} and {ALIGNED_IMAGES_DIR} being created if they dont exist")
+    landmarks_model_path = unpack_bz2(get_file('shape_predictor_68_face_landmarks.dat.bz2',
+                                               LANDMARKS_MODEL_URL, cache_subdir='temp'))
+#     RAW_IMAGES_DIR = sys.argv[1]
+#     ALIGNED_IMAGES_DIR = sys.argv[2]
+    all_important_info = []
+    landmarks_detector = LandmarksDetector(landmarks_model_path)
+    for img_name in os.listdir(RAW_IMAGES_DIR):
+        raw_img_path = os.path.join(RAW_IMAGES_DIR, img_name)
+        for i, face_landmarks in enumerate(landmarks_detector.get_landmarks(raw_img_path), start=1):
+            face_img_name = '%s_%02d.png' % (os.path.splitext(img_name)[0], i)
+            aligned_face_path = os.path.join(ALIGNED_IMAGES_DIR, face_img_name)
+
+            quad = image_align(raw_img_path, aligned_face_path, face_landmarks)
+            all_important_info.append((os.path.splitext(img_name)[0], i, quad))
+            print(f"adding {(os.path.splitext(img_name)[0], i, quad)}") 
+    return all_important_info
 
 if __name__ == "__main__":
     """
