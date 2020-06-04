5f1842ec0bb30a20e1d34463557af34cdad26b5f
ralcant <ralcanta@mit.edu>
2020-06-04 15:44:09 -0400

    fixed encoded file to have a function being called

--- encode_images.py
+++ encode_images.py
@@ -9,15 +9,100 @@ import dnnlib.tflib as tflib
 import config
 from encoder.generator_model import Generator
 from encoder.perceptual_model import PerceptualModel
+import datetime
+# URL_FFHQ = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ'  # karras2019stylegan-ffhq-1024x1024.pkl
 
-URL_FFHQ = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ'  # karras2019stylegan-ffhq-1024x1024.pkl
-
+URL_FFHQ = 'https://drive.google.com/uc?id=1PrjiBIXQ-tEa3l3vAtBR7FLi-n9WnVB8' #personal 
 
 def split_to_batches(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
+"""
+Find latent representation of reference images using perceptual loss
+"""
+def encode(src_dir,                 #Directory with images for encoding'
+           generated_images_dir,    #Directory with images for encoding
+           dlatent_dir,             #Directory for storing dlatent representations
+           batch_size=1,            #Batch size for generator and perceptual model
+           image_size= 256,         #Size of images for perceptual model
+           lr=1,                    #Learning rate for perceptual model
+           iterations=1000,         #Number of optimization steps for each batch
+           randomize_noise = False,#Add noise to dlatents during optimization
+           lazy_update = True):    #to not do more work than it needs
+    encode_start = datetime.datetime.now()
 
+    ref_images = [os.path.join(src_dir, x) for x in os.listdir(src_dir)]
+    ref_images = list(filter(os.path.isfile, ref_images))
 
+    if len(ref_images) == 0:
+        raise Exception('%s is empty' % src_dir)
+
+    os.makedirs(generated_images_dir, exist_ok=True)
+    os.makedirs(dlatent_dir, exist_ok=True)
+
+    # Initialize generator and perceptual model
+    tflib.init_tf()
+    with dnnlib.util.open_url(URL_FFHQ, cache_dir=config.cache_dir) as f:
+        generator_network, discriminator_network, Gs_network = pickle.load(f)
+
+    generator = Generator(Gs_network, batch_size, randomize_noise=randomize_noise)
+    perceptual_model = PerceptualModel(image_size, layer=9, batch_size=batch_size)
+    perceptual_model.build_perceptual_model(generator.generated_image)
+
+    # Optimize (only) dlatents by minimizing perceptual loss between reference and generated images in feature space
+    batch_times = []
+    batch_num = 0
+    for images_batch in tqdm(split_to_batches(ref_images, batch_size), total=len(ref_images)//batch_size):
+        batch_num+=1
+        start = datetime.datetime.now()
+        names = [os.path.splitext(os.path.basename(x))[0] for x in images_batch]
+        
+        if lazy_update:
+            if all(
+                os.path.exists(os.path.join(generated_images_dir, f'{img_name}.png'))
+                and os.path.exists(os.path.join(dlatent_dir, f'{img_name}.npy'))
+                for img_name in names): 
+                
+                print(f"lazy update = {lazy_update} and all the images/.npy files for {names} already exist, so we are skipping this")
+                continue
+
+        perceptual_model.set_reference_images(images_batch)
+#         op = perceptual_model.optimize(generator.dlatent_variable, iterations=iterations, learning_rate=lr)
+        for loss in tqdm(perceptual_model.optimize(generator.dlatent_variable, iterations=iterations, learning_rate=lr),
+                         total=iterations,
+                         desc = ' '.join(names)
+                        ):
+            final_loss = loss
+                         
+#         pbar = tqdm(op, leave=False, total=iterations)
+#         pbar.set_description(' '.join(names))
+#         for loss in pbar:
+# #             pbar.set_description(' '.join(names)+' Loss: %.2f' % loss)
+#               final_loss = loss
+        print(' '.join(names), ' loss:', final_loss)
+
+        # Generate images from found dlatents and save them
+        generated_images = generator.generate_images()
+        generated_dlatents = generator.get_dlatents()
+        for img_array, dlatent, img_name in zip(generated_images, generated_dlatents, names):
+            img = PIL.Image.fromarray(img_array, 'RGB')
+            img.save(os.path.join(generated_images_dir, f'{img_name}.png'), 'PNG')
+            np.save(os.path.join(dlatent_dir, f'{img_name}.npy'), dlatent)
+
+        generator.reset_dlatents()
+        end = datetime.datetime.now()
+        batch_times.append(end-start) #saving the time it took to process this batch
+        print(f"Batch #{batch_num} took {end-start}")
+    encode_end = datetime.datetime.now()
+    print("Finished, yayyyy")
+    print(f"total time = {encode_end-encode_start}")
+    if len(batch_times) > 0:
+        print(f"min batch time = {min(batch_times)}")
+        print(f"max batch time = {max(batch_times)}")
+    else:
+        print("no batches were run this time!")
+           
+           
 def main():
     parser = argparse.ArgumentParser(description='Find latent representation of reference images using perceptual loss')
     parser.add_argument('src_dir', help='Directory with images for encoding')
