6b5dae3ea4e0ee011e83978a8ebfe3ff72f82e37
ralcant <ralcanta@mit.edu>
2020-06-05 07:05:53 -0400

    added configuration of paths

---
+++ video_paths_config.py
@@ -0,0 +1,17 @@
+
+# Paths
+#--------------------------------------------------#
+
+general_videos_folder = "./videos/"                # where everything related to the videos is
+inputs_subfolder = "inputs/"                       # where all videos are
+frames_subfolder = "frames/"                       # where the frames of the original video are
+aligned_frames_subfolder = "aligned_frames/"       # where the aligned frames of the original video are
+generated_images_subfolder = "generated_images/"   # where the generated images of the aligned frames are
+latent_representations_subfolder = "latent_repr/"  # where the latent_representations of the aligned frames are
+processed_aligned_frames_subfolder = "processed_aligned_frames/" #where the changed aligned frames are 
+processed_frames_subfolder = "processed_frames/"   # where the final processed frames are 
+no_sound_videos_subfolder = "no_sound_videos/"     # where the output videos from the processed frames are
+audio_subfolder = "audios/"                        # where the mp3 of all videos are
+final_videos_subfolder = "final_videos/"           # where the final videos with sound are
+
+#--------------------------------------------------#
